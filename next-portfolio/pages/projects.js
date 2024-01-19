9f80507b4d35ba6e3e3a70ee96e1c1441ae245c9
dongdongdongha <06dongha@naver.com>
2024-01-20 00:35:27 +0900

    aa

--- next-portfolio/pages/projects.js
+++ next-portfolio/pages/projects.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
 import Head from "next/head";
+import { TOKEN, DATABASE_ID } from "../config";
 
 export default function Project() {
   return (
@@ -11,4 +12,35 @@ export default function Project() {
       </Head>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  const result = await res.json();
+  const projects = await res.json();
+
+  const projectIds = projects.results.map((project) => {
+    aProject.id;
+  });
+
+  console.log(`projectIds : ${projectIds}`);
+
+  return {
+    props: {},
+  };
 }
