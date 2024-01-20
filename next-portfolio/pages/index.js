4e0b9f497b1d376b4e34e86907df51a96cd5bd7d
dongdongdongha <06dongha@naver.com>
2024-01-21 00:47:04 +0900

    aa

--- next-portfolio/pages/index.js
+++ next-portfolio/pages/index.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>포트폴리오</title>
+        <title>똥트폴리오</title>
         <meta name="description" content="오늘도 빡코딩!" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/aaa.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
