53a286b03b9d32ad9399d750eae8381dbf47ff13
dongdongdongha <06dongha@naver.com>
2024-01-21 00:00:01 +0900

    ddaaa

--- next-portfolio/components/projects/project-item.js
+++ next-portfolio/components/projects/project-item.js
@@ -1,17 +1,70 @@
 import Image from "next/image";
 
-export default function ProjectItems({ data }) {
-  const title = data.peoperties.이름.title[0].plain_text;
-  const github = data.properties.Github.url;
-  const discription = data.properties.설명.rich_text[0].plain_text;
-  const imgsrc = data.cover.file?.url || data.cover.external.url;
+export default function ProjectItems({ result }) {
+  const title = result.peoperties.이름.title[0].plain_text;
+  const github = result.properties.Github.url;
+  const discription = result.properties.설명.rich_text[0].plain_text;
+  const imgsrc = result.cover.file?.url || result.cover.external.url;
+  const tags = result.peoperties.태그.multi_select;
+  const startDate = result.properties.WorkPeriod.date.start;
+  const endDate = result.properties.WorkPeriod.date.end;
+
+  const calculatePeriod = (start, end) => {
+    const startDateStringArray = start.split("-");
+    const endDateStringArray = end.split("-");
+
+    var startDate = new Date(
+      startDateStringArray[0],
+      startDateStringArray[1],
+      startDateStringArray[2]
+    );
+    var endDate = new Date(
+      endDateStringArray[0],
+      endDateStringArray[1],
+      endDateStringArray[2]
+    );
+
+    console.log(`startDate: ${startDate}`);
+    console.log(`endDate: ${endDate}`);
+
+    const diffInMs = Math.abs(endDate - startDate);
+    const result = diffInMs / (1000 * 60 * 60 * 24);
+
+    console.log(`기간 : ${result}`);
+    return result;
+  };
 
   return (
-    <div className="p-6 bg-slate-700 rounded-md">
-      <Image src={imgsrc} width="100%" height="60%" />
-      <h1>{title}</h1>
-      <h4>{discription}</h4>
-      <a href="{github}">깃허브 바로가기</a>
+    <div className="project-card">
+      <Image
+        className="rounded-t-xl"
+        src={imgsrc}
+        alt="cover image"
+        width="100%"
+        height="60%"
+        layout="responsive"
+        objectFit="contain"
+      />
+      <div className="p-4 flex flex-col">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <h3 mt-4 text-xl>
+          {discription}
+        </h3>
+        <a href="{github}">깃허브 바로가기</a>
+        <h3 className="mt-4 text-xl">
+          작업기간 : {start} ~ {end}({calculatePeriod(start, end)}일)
+        </h3>
+        <div className="flex items-start mt-2">
+          {tags.map((aTag) => (
+            <h1
+              className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700"
+              key={aTag.id}
+            >
+              {aTag.name}
+            </h1>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- next-portfolio/pages/projects.js
+++ next-portfolio/pages/projects.js
@@ -6,17 +6,24 @@ import { TOKEN, DATABASE_ID } from "../config";
 export default function Project({ projects }) {
   return (
     <Layout>
-      <Head>
-        <title>포트폴리오</title>
-        <meta name="description" content="오늘도 빡코딩!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className="flex flex-col items-center justify-center min-h-screen mb-10 px-6">
+        <Head>
+          <title>포트폴리오</title>
+          <meta name="description" content="오늘도 빡코딩!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <h1>총 프로젝트 : {projects.results.length}</h1>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트 : {projects.results.length}
+          <span className="pl-4 text-blue-500"></span>
+        </h1>
 
-      {projects.results.map((aProject) => {
-        <ProjectItems key={aProject.id} data={aProject} />;
-      })}
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 w-full">
+          {projects.results.map((aProject) => {
+            <ProjectItems key={aProject.id} data={aProject} />;
+          })}
+        </div>
+      </div>
     </Layout>
   );
 }
